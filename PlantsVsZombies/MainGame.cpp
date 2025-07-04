6f957d6888736053f86910a4f408e3ab2ffa961f
JINKYU-KIM-516 <qlika0516@naver.com>
2025-07-04 22:08:51 +0900

    ManagerManager 일단 성공

--- PlantsVsZombies/MainGame.cpp
+++ PlantsVsZombies/MainGame.cpp
@@ -6,38 +6,14 @@ MainGame::MainGame(HWND p_hWnd)
 	m_hWnd = p_hWnd;
 
 	m_managerManager = new ManagerManager(this);
-	/*
-	m_gameBoard = new GameBoard();
-	m_plantManager = new PlantManager();
-	m_sunlightManager = new SunlightManager();
-	m_bulletManager = new BulletManager();
-	m_zombieManager = new ZombieManager();
-	m_collisionManager = new CollisionManager();
-	m_store = new Store();
-	m_player = new Player();
 
-	m_gameBoard->Link(this);
-	m_plantManager->Link(m_sunlightManager ,m_bulletManager);
-	m_sunlightManager->Link(this);
-	m_collisionManager->Link(m_plantManager, m_zombieManager, m_bulletManager);
-	m_store->Link(this);
-	m_player->Link(this);
-	*/
 	m_debugTextIndex = GAMEBOARD_START_Y + (TILE_HEIGHT * GAMEBOARD_HEIGHT);
 	test();
 }
 
 MainGame::~MainGame()
 {
-	/*
-	delete m_gameBoard;
-	delete m_plantManager;
-	delete m_sunlightManager;
-	delete m_bulletManager;
-	delete m_zombieManager;
-	delete m_collisionManager;
-	delete m_store;
-	delete m_player;*/
+	delete m_managerManager;
 }
 
 void MainGame::test()
@@ -52,7 +28,7 @@ void MainGame::DebugTextOut(HDC hdc)
 {
 	int index = GAMEBOARD_START_Y + (TILE_HEIGHT * GAMEBOARD_HEIGHT);
 	int interval = 20;
-
+	/*
 	TCHAR str[20];
 	wsprintf(str, TEXT("햇빛 : %d"), m_player->GetSunlight());
 	TextOut(hdc, 0, index, str, lstrlen(str));
@@ -67,19 +43,11 @@ void MainGame::DebugTextOut(HDC hdc)
 	wsprintf(str2, TEXT("고른 식물 : %d"), m_player->GetSelectedCode());
 	TextOut(hdc, 0, index, str2, lstrlen(str2));
 	index += interval;
+	*/
 }
 
 void MainGame::Update()
 {
-	/*
-	m_plantManager->Update();
-	m_sunlightManager->Update();
-	m_bulletManager->Update();
-	m_zombieManager->Update();
-	m_collisionManager->Update();
-	m_store->Update();
-	m_player->Update();
-	*/
 	m_managerManager->Update();
 	if (m_clickOccured)
 	{
@@ -92,47 +60,12 @@ void MainGame::Update()
 
 void MainGame::ClickHandle()
 {
-	/*
-	m_sunlightManager->ClickHandle();
-	m_player->ClickHandle();
-	m_store->ClickHandle();
-	*/
+
 }
 
 void MainGame::DrawAll(HDC hdc)
 {
-	/*
-	if(m_gameBoard)
-	{
-		for (auto* tile : m_gameBoard->GetTiles())
-			tile->Draw(hdc);
-	}
-	if (m_store)
-	{
-		for (auto* image : m_store->GetImages())
-			image->Draw(hdc);
-	}
-	if (m_plantManager)
-	{
-		for (auto* plant : m_plantManager->GetPlants())
-			plant->Draw(hdc);
-	}
-	if (m_bulletManager)
-	{
-		for (auto* bullet : m_bulletManager->GetBullets())
-			bullet->Draw(hdc);
-	}
-	if (m_zombieManager)
-	{
-		for (auto* zombie : m_zombieManager->GetZombies())
-			zombie->Draw(hdc);
-	}
-	if (m_sunlightManager)
-	{
-		for (auto* sunlight : m_sunlightManager->GetSunlights())
-			sunlight->Draw(hdc);
-	}
-	*/
+
 	m_managerManager->Draw(hdc);
 	DebugTextOut(hdc);
 }
@@ -141,19 +74,16 @@ GameBoard* MainGame::GetGameBoard()
 {
 	
 	return dynamic_cast<GameBoard*>(m_managerManager->GetManagers()[GAMEBOARD_INDEX]);
-	//return m_gameBoard;
 }
 
 Player* MainGame::GetPlayer()
 {
 	return dynamic_cast<Player*>(m_managerManager->GetManagers()[PLAYER_INDEX]);
-	//return m_player;
 }
 
 PlantManager* MainGame::GetPlantManager()
 {
 	return dynamic_cast<PlantManager*>(m_managerManager->GetManagers()[PLANT_MANAGER_INDEX]);
-	//return m_plantManager;
 }
 
 Point MainGame::GetMousePosition()
